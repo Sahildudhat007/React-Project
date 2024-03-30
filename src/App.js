ae81d613b3af976d603395ec45d4025a2c5e82fb
Sahil <sahildudhat1919@gmail.com>
2024-03-30 09:06:39 +0530

    30-03-2024

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@
 // import Greeting from './Component/Greeting';
 // import Img from './Component/Img';
 import PropsCrad from "./Component/PropsCrad";
+import PropsData from "./Component/PropsData";
 
 function App() {
   return (
     <div className="App">
-    <div className="flex flex-wrap items-center justify-around mt-5">
+      <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center' }}>
+
+        {PropsData.map((cvalue, index, array) => {
+          console.log(cvalue, "cvalue")
+          return <PropsCrad key={index} name={cvalue.name} discription={cvalue.discription} coures={cvalue.coures} button={cvalue.button} image={cvalue.image} />
+        })}
+
+      </div>
+
+
+      {/* <div className="flex flex-wrap items-center justify-around mt-5"> */}
       {/* <PropsCrad/> */}
 
-      <PropsCrad name="Sahil Dudhat" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
-      <PropsCrad name="Piyush Suhagiya" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
-      <PropsCrad name="Jayan Dudakiya" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
-      <PropsCrad name="Nikhil Kalsariya" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
-      <PropsCrad name="Smit Thummar" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
-      <PropsCrad name="Akshay Goti" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
+      {/* <PropsCrad name="Sahil Dudhat" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
+        <PropsCrad name="Piyush Suhagiya" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
+        <PropsCrad name="Jayan Dudakiya" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
+        <PropsCrad name="Nikhil Kalsariya" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
+        <PropsCrad name="Smit Thummar" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/>
+        <PropsCrad name="Akshay Goti" coures="frontend developer" discription="The lorem text the section contain conatins header having open andclose functionality" button="View More" image="https://xsgames.co/randomusers/avatar.php?g=male"/> */}
 
       {/* <Img/>
-      <Greeting/> */}
-    </div>
+        <Greeting/> */}
     </div>
   );
 }
---
+++ src/Component/PropsData.js
@@ -0,0 +1,49 @@
+// import React from 'react'
+import image from '../assets/profile.png'
+
+const PropsData = [
+    {
+        name: "Sahil Dudhat",
+        discription: "Frontend Developer",
+        coures: "web design",
+        button: "View More",
+        image: image
+    },
+    {
+        name: "Sahil Dudhat",
+        discription: "Backend Developer",
+        coures: "web design",
+        button: "View More",
+        image: image
+    },
+    {
+        name: "Sahil Dudhat",
+        discription: "UI/UX designer",
+        coures: "web design",
+        button: "View More",
+        image: image
+    },
+    {
+        name: "Sahil Dudhat",
+        discription: "Frontend Developer",
+        coures: "web design",
+        button: "View More",
+        image: image
+    },
+    {
+        name: "Sahil Dudhat",
+        discription: "Backend Developer",
+        coures: "web design",
+        button: "View More",
+        image: image
+    },
+    {
+        name: "Sahil Dudhat",
+        discription: "UI/UX designer",
+        coures: "web design",
+        button: "View More",
+        image: image
+    },
+]
+
+export default PropsData
